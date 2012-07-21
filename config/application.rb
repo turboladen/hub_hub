f874976db2e4ade99bfdadc411bb50f15ea2d57d
Steve Loveless <steve.loveless@gmail.com>
2012-07-21 02:20:40 -0700

    Changing assets for heroku...

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module HubHub
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # For heroku...
+    config.assets.initialize_on_precompile = false
   end
 end
