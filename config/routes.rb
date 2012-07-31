cc7421176c38800ee4d839c1fdd4aba88b36604e
Steve Loveless <steve.loveless@gmail.com>
2012-07-30 23:41:59 -0700

    Use home path

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ HubHub::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'home#index', as: 'home'
 
   # See how all your routes lay out with "rake routes"
 
