274946c210216768509fd2fbd419be1cabfd7c54
Steve Loveless <steve.loveless@gmail.com>
2012-07-30 23:27:11 -0700

    Reordered definitions

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,14 @@
 class Post < ActiveRecord::Base
-  acts_as_commentable
-
-  belongs_to :spoke
   attr_accessible :content, :name, :title
 
+  belongs_to :spoke
   has_many :comments
 
   validates :name, presence: true
   validates :title, presence: true
   validates :content, presence: true
+
+  acts_as_commentable
 
   def root_comments
     self.comment_threads.where(parent_id: nil)
