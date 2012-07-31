25548f4f0989028056af507fdb5f52ffa99b88fd
Steve Loveless <steve.loveless@gmail.com>
2012-07-30 23:04:35 -0700

    First unit test for post

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    post = Post.new
+    assert post.invalid?
+    assert post.errors[:name].any?
+    assert post.errors[:title].any?
+    assert post.errors[:content].any?
+  end
 end
