de8bee0cf795097a3553847889e4f8198ad880c5
Steve Loveless <steve.loveless@gmail.com>
2012-07-31 21:47:02 -0700

    Moved root to top of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 HubHub::Application.routes.draw do
+  root :to => 'home#index', as: 'home'
+
   devise_for :users
 
   resources :spokes do
@@ -56,13 +58,4 @@ HubHub::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  root :to => 'home#index', as: 'home'
-
-  # See how all your routes lay out with "rake routes"
-
-  # This is a legacy wild controller route that's not recommended for RESTful applications.
-  # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
 end
