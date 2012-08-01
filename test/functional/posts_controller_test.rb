083724612be98ec15a82d364b7770fef8561ded6
Steve Loveless <steve.loveless@gmail.com>
2012-08-01 00:10:27 -0700

    Added tests for show

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = posts(:one)
+  end
+
+  test "should show post" do
+    get :show, id: @post, spoke_id: @post.spoke.id
+    assert_response :success
+    assert_select '.breadcrumb li', @post.spoke.name
+    assert_select 'h1', @post.title
+    assert_select '.span8 div p', /#{@post.created_at}/
+    assert_select '.span8 div p', /#{@post.name}/
+    assert_select '.span8 div p a', 'Edit'
+    assert_select '.span8 .well p', @post.content
+    assert_select 'td .comment', 1
+  end
 end
