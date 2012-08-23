4d9902b8d2836549f3ca79f346e8b807ab5c4068
Steve Loveless <steve.loveless@gmail.com>
2012-08-22 21:45:02 -0700

    Added tests for scopes

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
+  setup do
+    @one = posts(:one)
+    @two = posts(:two)
+    @three = posts(:three)
+    @four = posts(:four)
+    @five = posts(:five)
+  end
+
   test "post attributes must not be empty" do
     post = Post.new
     assert post.invalid?
@@ -26,5 +34,40 @@ class PostTest < ActiveSupport::TestCase
     post = Post.new(content: "c" * 4001, title: "1")
     assert !post.save
     assert_equal "is too long (maximum is 4000 characters)", post.errors[:content].join('; ')
+  end
+
+  test "#most_active returns only posts with comments" do
+    assert !Post.most_active.include?(@four)
+    assert !Post.most_active.include?(@five)
+  end
+
+  test "#most_active returns posts ordered by comment count" do
+    assert_equal Post.most_active, [@two, @three, @one]
+  end
+
+  test "#most_negative returns only posts with downvotes" do
+    assert !Post.most_negative.include?(@one)
+    assert !Post.most_negative.include?(@five)
+  end
+
+  test "#most_negative returns posts ordered by downvotes" do
+    assert_equal Post.most_negative, [@three, @two, @four]
+  end
+
+  test "#most_positive returns only posts with upvotes" do
+    assert !Post.most_positive.include?(@three)
+    assert !Post.most_positive.include?(@five)
+  end
+
+  test "#most_positive returns posts ordered by upvotes" do
+    assert_equal Post.most_positive, [@one, @two, @four]
+  end
+
+  test "#most_voted returns only posts with votes" do
+    assert !Post.most_voted.include?(@five)
+  end
+
+  test "#most_voted returns posts ordered by total votes" do
+    assert_equal Post.most_voted, [@two, @three, @one, @four]
   end
 end
