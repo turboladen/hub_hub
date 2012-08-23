9fd08543ac5ba907334be768f61fae96920fcaea
Steve Loveless <steve.loveless@gmail.com>
2012-08-22 22:08:23 -0700

    Added tests for validations

--- test/unit/comment_test.rb
+++ test/unit/comment_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "comment attributes must not be empty" do
+    comment = Comment.new
+    assert comment.invalid?
+    assert comment.errors[:body].empty?
+    assert comment.errors[:user].empty?
+  end
 end
