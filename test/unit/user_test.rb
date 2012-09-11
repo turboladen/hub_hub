43a8b5a7e66835d0e577cdaca65d17f8948bb81e
Steve Loveless <steve.loveless@gmail.com>
2012-09-10 23:01:45 -0700

    Refactored to use instance var for fixture

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  setup do
+    @bob = users(:bob)
+  end
+
   test "#name returns the first_name and last_name as a String" do
-    bob = users(:bob)
-    assert_equal(bob.name, "Bob Uecker")
+    assert_equal(@bob.name, "Bob Uecker")
+  end
   end
 end
