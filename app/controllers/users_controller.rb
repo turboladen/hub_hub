ada6da0bb822def15d85f90c10a5266d25a74f35
Steve Loveless <steve.loveless@gmail.com>
2012-09-15 12:57:05 -0700

    This shouldn't try to render anything

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,6 @@ class UsersController < ApplicationController
     make_admin = params[:is_admin] == "true" ? true : false
     @user = User.find(params[:id])
     @user.update_attribute :admin, make_admin
+    render :nothing => true
   end
 end
