d500f0dc2b9306e1754e744ffc876619650b5549
Steve Loveless <steve.loveless@gmail.com>
2012-09-15 16:07:43 -0700

    Admin pages are now red

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def tag_with_color(tag)
+    capture_haml do
+      if @background_color.nil?
+        haml_tag tag do
+          yield
+        end
+      else
+        haml_tag tag, { style: "background-color: #{@background_color}" } do
+          yield
+        end
+      end
+    end
+  end
 end
