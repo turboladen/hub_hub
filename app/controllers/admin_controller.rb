9ea95451102dbb9ebd352449a3d824d0d03bbcc0
Steve Loveless <steve.loveless@gmail.com>
2012-09-16 13:33:42 -0700

    Added flaggings view for admin to render that

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -18,6 +18,8 @@ class AdminController < ApplicationController
         title: post.title
       }
     end
+
+    render 'flaggings/index'
   end
 
   def users
---
+++ test/unit/helpers/flaggings_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class FlaggingsHelperTest < ActionView::TestCase
+end
