b433ce1383fc55a155f8649b722f96a8916c7af0
Steve Loveless <steve.loveless@gmail.com>
2012-09-17 22:41:14 -0700

    Prompting about flagging posts now down via bootstrap modal. Closes #28

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,16 @@ module ApplicationHelper
       end
     end
   end
+
+  # @return [String] The message that should be shown to users when they click
+  #   the button to flag a post or comment as inappropriate.
+  def inappropriate_confirm_message
+    msg = <<-MSG
+Are you sure you want to flag this as inappropriate?  Flagging this will notify
+moderators that you've done so.  Abusing these flags will be noted and may lead
+to banning from the site.
+    MSG
+
+    msg.gsub("\n", ' ')
+  end
 end
