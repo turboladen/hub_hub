9c3d51c471b0efc04b25f4e9926a8e718f0fb506
Steve Loveless <steve.loveless@gmail.com>
2012-10-10 01:12:36 -0700

    Added test for index + sorter, TOS

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,11 +1,31 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
-
+  test "gets index" do
     get :index
+
+    assert_not_nil assigns(:posts)
+    assert_equal assigns(:sort_options), Post.sort_options
+    assert_not_nil assigns(:spokes)
+
     assert_response :success
     assert_select 'li .spoke', 2
     assert_select 'td .post-title', 6
+  end
+
+  test "gets index with sorter" do
+    Post.expects(:newest)
+    get :index, sort: :newest
+
+    assert_response :success
+    assert_select 'li .spoke', 2
+    assert_select 'td .post-title', 6
+  end
+
+  test "gets terms of service" do
+    get :tos
+
+    assert_response :success
+    assert_template 'home/tos'
   end
 end
