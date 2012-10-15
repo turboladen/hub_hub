6f1a9a72e5371251bd3f4133258fab820921b340
Steve Loveless <steve.loveless@gmail.com>
2012-10-14 23:42:41 -0700

    Added tests for making admin and banning

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -43,10 +43,15 @@ class AdminController < ApplicationController
 
     if params["#{@user.id}-is-admin"]
       @make_admin = params["#{@user.id}-is-admin"] == "true" ? true : false
-      @user.update_attribute :admin, @make_admin
 
-      respond_to do |format|
-        format.js { render 'update_user_admin' }
+      if @user.update_attribute :admin, @make_admin
+        respond_to do |format|
+          format.js { render 'update_user_admin' }
+        end
+      else
+        respond_to do |format|
+          format.js { render nothing: true, status: :inprocessable_entity }
+        end
       end
 
       return
@@ -54,11 +59,17 @@ class AdminController < ApplicationController
 
     if params["#{@user.id}-is-banned"]
       @ban_user = params["#{@user.id}-is-banned"] == "true" ? true : false
-      @user.update_attribute :banned, @ban_user
-
-      respond_to do |format|
-        format.js { render 'update_user_ban' }
+      
+      if @user.update_attribute :banned, @ban_user
+        respond_to do |format|
+          format.js { render 'update_user_ban' }
+        end
+      else
+        respond_to do |format|
+          format.js { render nothing: true, status: :inprocessable_entity }
+        end
       end
+
       return
     end
 
--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,18 +1,22 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
+  setup do
+    @bob = users(:bob)
+  end
+
   test "must be logged in as an admin to view inappropriate items" do
     assert_raise ActionController::RoutingError do
       get :inappropriate_items
     end
 
-    sign_in users(:bob)
+    sign_in @bob
 
     assert_raise ActionController::RoutingError do
       get :inappropriate_items
     end
 
-    sign_out users(:bob)
+    sign_out @bob
     sign_in users(:admin)
     assert_nothing_raised do
       get :inappropriate_items
@@ -20,12 +24,119 @@ class AdminControllerTest < ActionController::TestCase
   end
 
   test "shows posts flagged as inappropriate" do
-    users(:bob).flag posts(:post_one), :inappropriate
+    @bob.flag posts(:post_one), :inappropriate
     sign_in users(:admin)
     get :inappropriate_items
 
     assert_equal 1, assigns(:flags_and_posts).size
     assert_template 'flaggings/index'
+  end
+
+  test "must be logged in as an admin to view users" do
+    assert_raise ActionController::RoutingError do
+      get :index_users
+    end
+
+    sign_in @bob
+
+    assert_raise ActionController::RoutingError do
+      get :index_users
+    end
+
+    sign_out @bob
+    sign_in users(:admin)
+    assert_nothing_raised do
+      get :index_users
+    end
+  end
+
+  test "must be logged in as an admin to view user" do
+    assert_raise ActionController::RoutingError do
+      get :show_user, id: @bob.id
+    end
+
+    sign_in @bob
+
+    assert_raise ActionController::RoutingError do
+      get :show_user, id: @bob.id
+    end
+
+    sign_out @bob
+    sign_in users(:admin)
+    assert_nothing_raised do
+      get :show_user, id: @bob.id
+    end
+  end
+
+  test "must be logged in as an admin to update user" do
+    assert_raise ActionController::RoutingError do
+      xhr :put, :update_user, id: @bob.id, "#{@bob.id}-is-admin" => "true"
+    end
+
+    sign_in @bob
+
+    assert_raise ActionController::RoutingError do
+      xhr :put, :update_user, id: @bob.id, "#{@bob.id}-is-admin" => "true"
+    end
+
+    sign_out @bob
+    sign_in users(:admin)
+    assert_nothing_raised do
+      xhr :put, :update_user, id: @bob.id, "#{@bob.id}-is-admin" => "true"
+    end
+
+    assert_response 200
+  end
+
+  test "can make user an admin" do
+    sign_in users(:admin)
+    assert !@bob.admin?
+
+    xhr :put, :update_user, id: @bob.id, "#{@bob.id}-is-admin" => "true"
+
+    assert assigns(:make_admin)
+    assert_response 200
+    assert_template 'update_user_admin'
+    updated_bob = User.find(@bob.id)
+    assert updated_bob.admin?
+  end
+
+  test "can make an admin a regular user" do
+    sign_in users(:admin)
+    assert !@bob.admin?
+    @bob.update_attribute :admin, true
+
+    xhr :put, :update_user, id: @bob.id, "#{@bob.id}-is-admin" => "false"
+
+    assert !assigns(:make_admin)
+    assert_response 200
+    updated_bob = User.find(@bob.id)
+    assert !updated_bob.admin?
+  end
+
+  test "can ban user" do
+    sign_in users(:admin)
+    assert !@bob.banned?
+
+    xhr :put, :update_user, id: @bob.id, "#{@bob.id}-is-banned" => "true"
+
+    assert assigns(:ban_user)
+    assert_response 200
+    updated_bob = User.find(@bob.id)
+    assert updated_bob.banned?
+  end
+
+  test "can unban a banned user" do
+    sign_in users(:admin)
+    assert !@bob.banned?
+    @bob.update_attribute :banned, true
+
+    xhr :put, :update_user, id: @bob.id, "#{@bob.id}-is-banned" => "false"
+
+    assert !assigns(:ban_user)
+    assert_response 200
+    updated_bob = User.find(@bob.id)
+    assert !updated_bob.banned?
   end
 
   test "doesn't allow updating admin@mindhub.org" do
@@ -37,41 +148,5 @@ class AdminControllerTest < ActionController::TestCase
     assert_response 302
     assert_redirected_to admin_url
     assert_equal "Can't update super-user.", flash[:notice]
-  end
-
-  test "must be logged in as an admin to view users" do
-    assert_raise ActionController::RoutingError do
-      get :index_users
-    end
-
-    sign_in users(:bob)
-
-    assert_raise ActionController::RoutingError do
-      get :index_users
-    end
-
-    sign_out users(:bob)
-    sign_in users(:admin)
-    assert_nothing_raised do
-      get :index_users
-    end
-  end
-
-  test "must be logged in as an admin to view user" do
-    assert_raise ActionController::RoutingError do
-      get :show_user, id: users(:bob).id
-    end
-
-    sign_in users(:bob)
-
-    assert_raise ActionController::RoutingError do
-      get :show_user, id: users(:bob).id
-    end
-
-    sign_out users(:bob)
-    sign_in users(:admin)
-    assert_nothing_raised do
-      get :show_user, id: users(:bob).id
-    end
   end
 end
