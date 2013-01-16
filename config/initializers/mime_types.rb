a82521eeba2d9eb8f0ff9f6da59bbf0ba338ba7d
Steve Loveless <steve.loveless@gmail.com>
2013-01-15 21:08:01 -0800

    Disable XML parser for security fix

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -3,3 +3,7 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+# Fix for security bug
+# https://groups.google.com/forum/?fromgroups=#!topic/rubyonrails-security/61bkgvnSGTQ
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
