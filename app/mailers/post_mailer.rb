0b91f017f1ae69e0d8de9ddec34e91efac59d868
Steve Loveless <steve.loveless@gmail.com>
2013-01-19 14:47:44 -0800

    First attempt at receiving email for post.  Relates to gh-4

---
+++ app/mailers/post_mailer.rb
@@ -0,0 +1,23 @@
+class PostMailer < ActionMailer::Base
+  def receive(email)
+    user = User.find_by_address(email.from)
+
+    if user.nil?
+      logger.info "Received email from an unknown address: #{email.from}"
+      # Respond back to email.from saying they need to register
+    end
+
+    logger.info "Received email from user: #{email.from}"
+    match_data = email.subject.match(/(?<spoke_name>[\w+]):\s+(?<post_title>)/)
+    spoke = Spoke.find_by_name(match_data[:spoke])
+    post = spoke.posts.build(title: match_data[:post_title], content: email.body)
+    post.user = user
+
+    if post.save
+      post.tweet(spoke_post_url(spoke.id), post)
+    else
+      logger.debug post.errors.inspect
+      # Send out response email saying why the post creation failed
+    end
+  end
+end
