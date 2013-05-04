0580f3c39811aaae689de09f63f2419c67593679
Steve Loveless <steve.loveless@gmail.com>
2013-05-04 14:31:52 -0700

    Refactored out #vote method

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -5,33 +5,32 @@ class VotesController < ApplicationController
   VOTABLE_CLASSES = [Post, Comment]
 
   def upvote
-    @item_type = item_class(params[:item_type])
-    @item = @item_type.find params[:item_id]
-
-    if current_user.voted_up_on? @item
-      current_user.unvote_for @item
-    else
-      @item.liked_by current_user
-    end
-
-    @upvote_count = @item.likes.size.to_s
-    @downvote_count = @item.dislikes.size.to_s
-    @total_vote_count = (@upvote_count.to_i - @downvote_count.to_i).to_s
-
-    respond_to do |format|
-      format.js
+    vote do
+      if current_user.voted_up_on? @item
+        current_user.unvote_for @item
+      else
+        @item.liked_by current_user
+      end
     end
   end
 
   def downvote
+    vote do
+      if current_user.voted_down_on? @item
+        current_user.unvote_for @item
+      else
+        @item.downvote_from current_user
+      end
+    end
+  end
+
+  private
+
+  def vote(&block)
     @item_type = item_class(params[:item_type])
     @item = @item_type.find params[:item_id]
 
-    if current_user.voted_down_on? @item
-      current_user.unvote_for @item
-    else
-      @item.downvote_from current_user
-    end
+    block.call
 
     @upvote_count = @item.likes.size.to_s
     @downvote_count = @item.dislikes.size.to_s
@@ -41,8 +40,6 @@ class VotesController < ApplicationController
       format.js
     end
   end
-
-  private
 
   # Gets the class of the +param+ type.  Allows for voting on any objects that
   # are votable.
