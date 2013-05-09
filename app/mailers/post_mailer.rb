235827efa8d554b7c03f94a469627d0046fa3ab1
Steve Loveless <steve.loveless@gmail.com>
2013-05-09 15:56:53 -0700

    Fixed log message

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -8,7 +8,7 @@ class PostMailer < ActionMailer::Base
 
   def receive(email)
     user = if email.sender && email.sender == 'mindhub-list-bounces@list.mindhub.org'
-      log "Got listserv email from '#{email.from}'; sender: '#{email.sender}'"
+      logger.info "Got listserv email from '#{email.from}'; sender: '#{email.sender}'"
       User.list_recipient
     else
       User.find_by_email(email.from)
