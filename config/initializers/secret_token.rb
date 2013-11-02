73d94a78c4b801f72e9f7ec60a52485e4db46482
Steve Loveless <steve.loveless@gmail.com>
2013-11-02 09:51:48 -0700

    Initial Rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HubHub::Application.config.secret_key_base = '1d01f38591c4a126f31f8ab19651582e4b720d2c8f5377f09c26d62173b0245e8b569ae5dbd142d509b3c6a580baf71efee5bc12d8802907fa924f75f644fed1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HubHub::Application.config.session_store :cookie_store, key: '_hub_hub_session'
